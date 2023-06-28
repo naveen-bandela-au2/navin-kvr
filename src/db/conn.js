45db8c29408381f4916f90544983b3ae92c88b38
naveen-bandela-au2 <50129187+naveen-bandela-au2@users.noreply.github.com>
2023-06-28 22:15:56 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -4,7 +4,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log('Connection successfull!');
+    console.log('Connection successfull!',process.env.MONGO_URI);
 }).catch((e) => {
-    console.log('Connection failed!');
+    console.log('Connection failed!',process.env.MONGO_URI);
 })
