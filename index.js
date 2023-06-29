5c1e59c8462fa37ca003216eb0f0c225dcb1c99b
naveen-bandela-au2 <50129187+naveen-bandela-au2@users.noreply.github.com>
2023-06-29 19:46:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 require('./src/db/conn');
 const express = require("express");
-var cors = require('cors')
+const cors = require('cors');
 const userRoutes = require("./src/routes/userRoutes");
 
 const app = express();
 const port = process.env.PORT;
-app.use(cors());
+
 app.use(express.json({ limit: "10MB" }));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use('/', userRoutes);
 
 app.listen(port, () => {
