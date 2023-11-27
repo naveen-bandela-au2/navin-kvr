4838ef72867d24918ff75769c421467bcfd2cb16
naveen-bandela-au2 <50129187+naveen-bandela-au2@users.noreply.github.com>
2023-11-27 12:16:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json({ limit: "100MB" }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cors());
 app.use('/', userRoutes);
 
