b1e8bf3ee2882421794381d192f5f65cfbf499dc
naveen-bandela-au2 <50129187+naveen-bandela-au2@users.noreply.github.com>
2023-11-27 12:12:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const userRoutes = require("./src/routes/userRoutes");
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json({ limit: "10MB" }));
+app.use(express.json({ limit: "100MB" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/', userRoutes);
